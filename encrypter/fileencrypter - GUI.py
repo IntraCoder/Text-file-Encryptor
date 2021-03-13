b4c088bfce5fabe4e5c0ce140a6bdc098412a19f
IntraCoder <intracoder569@gmail.com>
2021-03-13 13:42:16 +0530

    Update fileencrypter - GUI.py

--- encrypter/fileencrypter - GUI.py
+++ encrypter/fileencrypter - GUI.py
@@ -2,10 +2,10 @@ from tkinter import *
 from tkinter import messagebox as msg
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
-from encrypter import fileencrypter_CLI
+from cryptography.fernet import Fernet, InvalidToken
 
 root = Tk()
-root.geometry("440x300")
+root.geometry("410x340")
 root.title("Encryptor")
 
 # TODO-> Importing images (you may remove image part )
@@ -29,14 +29,61 @@ def key_chooser():
     key_ent.insert(0, keyname)
 
 
+def file_work(file_name, purpose, data=None, truncate=False):
+    file = open(file_name, purpose)
+
+    if "r" in purpose:
+        content = file.read()
+        file.close()
+        return content
+
+    elif "w" in purpose:
+        if truncate:
+            file.truncate()
+        file.write(data)
+    file.close()
+
+
+# TODO -> Function for Encryption and Decryption
+
+def crypt(file_name, key_file, cryption, num):
+    key = file_work(key_file, "rb")
+    log_lb.config(text="[+] Fetching key...")
+    key = Fernet(key)
+    text = file_work(file_name, "rb")
+    log_lb.config(text="[+] Reading File...")
+
+    if cryption == "encrypt":
+        try:
+            log_lb.config(text="[+] Encrypting Data...")
+            for i in range(num):
+                text = key.encrypt(text)
+            log_lb.config(text="[+] Encryption Successful!")
+            file_work(file_name, "wb", text, True)
+        except UnicodeDecodeError:
+            log_lb.config(text="[-] Type Error : Cannot encrypt files of this type.")
+        except MemoryError:
+            log_lb.config(text="EOF Error : Encrypted text out of limit.")
+
+    elif cryption == "decrypt":
+        try:
+            log_lb.config(text="[+] Decrypting Data...")
+            for i in range(num):
+                text = key.decrypt(text)
+            file_work(file_name, "wb", text, True)
+            log_lb.config(text="[+] Decryption Successful!")
+        except InvalidToken:
+            log_lb.config(text="[+] Already Decrypted!")
+
+
 # TODO -> Function to encrypt
 def encrypt():
     if file_var.get() != "" and key_var.get() != "":  # Check if Entry box is not empty
         try:
-            fileencrypter_CLI.crypt(file_var.get(), key_var.get(), "encrypt")
-            msg.showinfo("Successful", f"Encryption of {file_var.get().split('/')[-1]} was successful")
+            crypt(file_var.get(), key_var.get(), "encrypt", time_var.get())
         except ValueError:  # Proper Key file must be input
-            msg.showerror("Error", "Fernet key must be 32 url-safe base64-encoded bytes.")
+            log_lb.config(text="[-] Error : Fernet key must be 32 url-safe base64-encoded bytes.")
+
     else:  # Nothing happens for empty entry
         pass
 
@@ -44,35 +91,42 @@ def encrypt():
 # TODO -> Function to decrypt
 def decrypt():
     if file_var.get() != "" and key_var.get() != "":  # Check if Entry box is not empty
-        try:
-            fileencrypter_CLI.crypt(file_var.get(), key_var.get(), "decrypt")
-            msg.showinfo("Successful", f"Decryption of {file_var.get().split('/')[-1]} was successful")
-        except ValueError:  # Proper Key file must be input
-            msg.showerror("Error", "Fernet key must be 32 url-safe base64-encoded bytes.")
+
+        crypt(file_var.get(), key_var.get(), "decrypt", time_var.get())
+        print(file_work(file_var.get(), "rb").decode())
     else:  # Nothing happens for empty entry
         pass
 
 
 Label(root, image=back).place(x=0, y=-50)
-
+Label(root, text="Times", font=("Bahnschrift SemiBold Condensed", 17)).place(x=50, y=180)
 file_var = StringVar()
 key_var = StringVar()
-
-file_ent = Entry(root, textvariable=file_var, font=("Bahnschrift SemiBold Condensed", 17))
-key_ent = Entry(root, textvariable=key_var, font=("Bahnschrift SemiBold Condensed", 17))
-
+time_var = IntVar()
+time_var.set(1)
+key_var.set("D:/Python-projects/encrypter/key_store.key")
+file_var.set("C:/Users/aniso/AppData/Local/Programs/Python/Python38-32/pythonnetroit.bin")
+file_ent = Entry(root, textvariable=file_var, font=("Bahnschrift SemiBold Condensed", 17), relief=SUNKEN, bd=3,
+                 width=23)
+key_ent = Entry(root, textvariable=key_var, font=("Bahnschrift SemiBold Condensed", 17), relief=SUNKEN, bd=3, width=23)
+times_ent = Entry(root, textvariable=time_var, font=("Bahnschrift SemiBold Condensed", 17), relief=SUNKEN, bd=3,
+                  width=23)
 encrypt_but = Button(root, text="Encrypt", height=1, borderwidth=0, background="grey25", font="None 17",
-                     foreground="white", command=encrypt, width=10)
+                     foreground="white", command=encrypt, width=10, relief=RAISED)
 decrypt_but = Button(root, text="Decrypt", height=1, borderwidth=0, background="grey25", font="None 17",
-                     foreground="white", command=decrypt, width=10)
-choose_file = Button(root, text="Choose File", image=fold, borderwidth=0, command=file_chooser)
-choose_key = Button(root, text="Choose Key", image=key_img, borderwidth=0, command=key_chooser)
+                     foreground="white", command=decrypt, width=10, relief=RAISED)
+choose_file = Button(root, text="Choose File", image=fold, command=file_chooser, relief=RAISED)
+choose_key = Button(root, text="Choose Key", image=key_img, command=key_chooser, relief=RAISED)
 
-file_ent.place(x=125, y=100)
-key_ent.place(x=125, y=170)
-choose_file.place(x=80, y=100)
-choose_key.place(x=80, y=170)
-encrypt_but.place(x=100, y=240)
-decrypt_but.place(x=250, y=240)
+log_lb = Label(root, relief=RAISED, font=("Bahnschrift SemiBold Condensed", 15), justify=LEFT, bd=4, width=54)
+log_lb.pack(side=BOTTOM)
+
+file_ent.place(x=125, y=80)
+key_ent.place(x=125, y=130)
+times_ent.place(x=125, y=180)
+choose_file.place(x=60, y=80)
+choose_key.place(x=60, y=130)
+encrypt_but.place(x=80, y=240)
+decrypt_but.place(x=230, y=240)
 
 root.mainloop()
